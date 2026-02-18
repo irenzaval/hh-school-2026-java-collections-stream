f817a6ee1d0b00aef68f7ba5961cab753446a6fb
irenzaval <elenazavaley1979@gmail.com>
2026-02-18 22:07:59 +0300

    Update Task1.java

--- src/main/java/tasks/Task1.java
+++ src/main/java/tasks/Task1.java
@@ -2,9 +2,13 @@ package tasks;
 
 import common.Person;
 import common.PersonService;
+
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
+import java.util.Map;
 
 /*
 Задача 1
@@ -23,6 +27,18 @@ public class Task1 {
 
   public List<Person> findOrderedPersons(List<Integer> personIds) {
     Set<Person> persons = personService.findPersons(personIds);
-    return Collections.emptyList();
+    List<Person> orderedPersons = new ArrayList<>();
+    Map<Integer,Person> mapPersons =new HashMap<>();
+    for (Person person : persons)
+    {
+      mapPersons.put(person.id(),person);
+    }
+    for (Integer id : personIds)
+    {
+      orderedPersons.add(mapPersons.get(id));
+    }
+    return orderedPersons;
+
   }
+  //время поиска по значению в карте O(n), по списку тоже O(n), поиск по индексу O(1),получается временная сложность O(2n)=O(n)
 }
