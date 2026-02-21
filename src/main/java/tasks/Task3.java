5fcbab0e1ee4bc4e80621f34eaac116cf45cef48
irenzaval <elenazavaley1979@gmail.com>
2026-02-21 22:41:58 +0300

    Update Task3.java

--- src/main/java/tasks/Task3.java
+++ src/main/java/tasks/Task3.java
@@ -1,11 +1,9 @@
 package tasks;
 
 import common.Person;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Comparator;
-
 /*
 Задача 3
 Отсортировать коллекцию сначала по фамилии, по имени (при равной фамилии), и по дате создания (при равных фамилии и имени)
